5c67017a31c83de5725f8f07a8af924972c69cca
admin <>
2017-03-09 16:42:11 +0800

    Initial commit

---
+++ app/src/main/java/mobile/xiyou/atest/TestActivity.java
@@ -0,0 +1,12 @@
+package mobile.xiyou.atest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by admin on 2017/3/1.
+ */
+
+public class TestActivity extends ActivityBase {
+}
